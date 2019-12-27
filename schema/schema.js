0361cdf004dcc4e19e06db397d5fe9ad7584947a
Shouvik Mukherjee <shouvik.shouvik@gmail.com>
2019-12-27 13:32:26 +0530

    #15 compolete

--- schema/schema.js
+++ schema/schema.js
@@ -63,7 +63,7 @@ const AuthorType = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return _.filter(books, {authorId: parent.id});
+        return _.filter(books, { authorId: parent.id });
       }
     }
   })
@@ -85,6 +85,18 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return _.find(authors, { id: args.id });
+      }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
       }
     }
   }
