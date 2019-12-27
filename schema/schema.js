2d5819927c4b49878e3f08aefaab9a33419bba56
Shouvik Mukherjee <shouvik.shouvik@gmail.com>
2019-12-27 14:15:43 +0530

    #17 Complete, mongoDB models

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require("graphql");
 const _ = require("lodash");
+const Book = require('../models/book');
+const Author = require('../models/author');
 
 const {
   GraphQLObjectType,
@@ -9,23 +11,6 @@ const {
   GraphQLID,
   GraphQLList
 } = graphql;
-
-// dummy data
-let books = [
-  { id: "1", name: "Name of the Wind", genre: "Fantasy", authorId: "1" },
-  { id: "2", name: "The Final Empire", genre: "Fantasy", authorId: "3" },
-  { id: "3", name: "The Long Earth", genre: "Sci-Fi", authorId: "1" },
-  { id: "4", name: "Harry Potter", genre: "Fantasy", authorId: "2" },
-  { id: "5", name: "Billboards", genre: "Fantasy", authorId: "3" },
-  { id: "6", name: "The Second Earth", genre: "Sci-Fi", authorId: "1" },
-  { id: "7", name: "Non Generic", genre: "Fantasy", authorId: "2" }
-];
-
-let authors = [
-  { id: "1", name: "Shouvik Mukherjee", age: 30 },
-  { id: "2", name: "Deepshikha Banerjee", age: 22 },
-  { id: "3", name: "Amit Kumar Poreli", age: 45 }
-];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
